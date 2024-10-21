9d7b1c618b580be0ea17befcfa54014b9f8f60f9
prajnapoojary <prajna.poojary@robosoftin.com>
2024-10-21 16:00:59 +0530

    Made changes

--- src/JavaDataTypes.java
+++ src/JavaDataTypes.java
@@ -3,23 +3,20 @@ public class JavaDataTypes {
         String name = "Jeevan";
         String S1 = "Mr.";
         String S2 = " Jain";
-//        String lastname = name.concat(S2);
-//        String fullname = S1.concat(lastname);
-        String fullName = S1+name+S2;
-        System.out.println("Name: " +name);
-        System.out.println("Fullname: " +fullName);
-
-
-//        String name ="Jeevan";
         int age= 70;
         float height=5.9f;
         double weight =50.443;
         boolean isEmployed = true;
         String[] skills = {"java","javascript","SQL"};
 
+        String fullName = S1+name+S2;
+        System.out.println("Name: " +name);
+        System.out.println("Fullname: " +fullName);
+
         System.out.println("no. of skills: " +skills.length);
 
-        System.out.println("Weight: "+Math.round(weight));
+        System.out.println("Weight: "+Math.round(weight)); 
+
         long ageInSeconds = age*(365L*24*60*60);
         System.out.println("Age in Seconds:" +ageInSeconds);
 
